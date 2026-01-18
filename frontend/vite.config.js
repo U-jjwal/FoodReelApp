a16ba4c229503af9e9c32c8f9aa531b0e4bfea66
U-jjwal <ujjwaltiwari242@gmail.com>
2026-01-18 18:51:31 +0530

    feat: add .env file to configure backend URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api':'http://localhost:5000',
-    }
+      "/api": {
+        target: import.meta.env.VITE_BACKEND_URL,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
-  plugins: [react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+});
