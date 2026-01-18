2d1173f1e1e048986dc5a00635a54bb30f27621d
U-jjwal <ujjwaltiwari242@gmail.com>
2026-01-18 19:31:26 +0530

    chore: update package dependencies and configuration for improved performance

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,3 +18,19 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: env.VITE_BACKEND_URL,
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react(), tailwindcss()],
+// });
