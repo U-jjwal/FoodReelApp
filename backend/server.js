1abd22ceb229b245ba5ffc9fe76f8bc837947eea
U-jjwal <ujjwaltiwari242@gmail.com>
2026-01-20 13:37:42 +0530

    chelcges

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import app from './src/app.js';
 connectdb()
 .then(()=> {
     app.listen(process.env.PORT, () => {
-        console.log('Server is running on port 3000');
+        console.log(`Server is running on port ${process.env.PORT}`);
     });
 })
 .catch((err) => {
